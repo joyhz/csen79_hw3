78a4992eb581f98465215902e0ce870a90649a45
joyhz <jhzhu@scu.edu>
2025-10-08 01:05:46 -0700

    moving top var declaration to the header file

--- bagtest.cxx
+++ bagtest.cxx
@@ -22,7 +22,6 @@ int main(void) {
         switch (line[0]) {
         case PUSH: {
             int data;
-            Data top = 0;    // To keep track of the stack's top element
             try {
                 stringstream ss(line.substr(1));
                 ss >> data;
