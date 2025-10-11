9cf9f5217ab43d79e961e279aed8a8cd626412f7
joyhz <jhzhu@scu.edu>
2025-10-10 22:18:38 -0700

    Update bagtest.cxx

--- bagtest.cxx
+++ bagtest.cxx
@@ -18,39 +18,37 @@ int main(void) {
     Bag bag;
     string line;
     bool quit = false;
-    top = 0;
     while (!quit && std::getline(cin,line)) {
         switch (line[0]) {
-        case PUSH: {
-            int data;
-            try {
-                stringstream ss(line.substr(1));
-                ss >> data;
-            } catch (const out_of_range &e) {
-                cerr << "Data out of range" << endl;
-                continue;
+            case PUSH: {
+                int data;
+                try {
+                    stringstream ss(line.substr(1));
+                    ss >> data;
+                } catch (const out_of_range &e) {
+                    cerr << "Data out of range" << endl;
+                    continue;
+                }
+                bag.push(data);
             }
-            bag.push(data);
-            top++;    //Might not need bc of the data[++top] in bag.cxx
-        }
-            break;
-        case POP: {
-            bag.pop();
-        }
-        case PRINT:{
-            cout << "Bag contents: ";
-            bag.print();
-            cout << endl;
-            cout << "Current size: " << bag.size() << endl;
-            break;
-        }
-            break;
-        case QUIT:
-            quit = true;
-            break;
-        default:
-            cerr << "Unknown command: " << line << endl;
-            break;
+                break;
+            case POP: {
+                int data = bag.pop();
+                cout << "Popped: " << data << endl;
+            }
+                break;
+            case PRINT: {
+                cout << "Bag contents: " << endl;
+                bag.print();
+                cout << "Current size: " << bag.size() << endl;
+            }
+                break;
+            case QUIT:
+                quit = true;
+                break;
+            default:
+                cerr << "Unknown command: " << line << endl;
+                break;
         }
     }
     
