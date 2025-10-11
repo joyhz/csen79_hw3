10a17aaea40054f7418d6bdb13fb51551b54fcc7
joyhz <jhzhu@scu.edu>
2025-10-10 23:20:17 -0700

    implemented isFull + fixed push

--- bag.cxx
+++ bag.cxx
@@ -37,7 +37,6 @@ namespace csen79 {
         return this->operator=(rhs);
     }
 
-
     // simple access functions
     // replace them with appropriate ones for assignments
     const Bag::Data &Bag::getData(const int i) const {
@@ -50,18 +49,26 @@ namespace csen79 {
         return top < 0;
     }
 
+    bool Bag::isFull() const {
+        return size() >= DATASIZE;  // Can't compare top w/ DATASIZE, compares int w/ size_t (unsigned int) --> have to convert top to size_t to compare
+    }
+
     std::size_t Bag::size() const {
         return static_cast<std::size_t>(top + 1);
     }
 
     void Bag::setData(const int i, const Data &d) {
-        if (i < 0 || i >= DATASIZE)
+        if (i < 0 || i >= DATASIZE)    // No issue comparing here; neg int --> throw, pos int --> simple comparison
             throw std::out_of_range("Index out of range");
         data[i] = d;
     }
     void Bag::push(const Data &d) {
-        if (top < -1 || top >= DATASIZE - 1)    // top < -1 checks for data corruption
+        if (top < -1) {
+            throw std::underflow_error("Data corruption detected");
+        }
+        if (isFull()) {
             throw std::overflow_error("Stack is full");
+        }
         data[++top] = d;
     };
     Bag::Data Bag::pop() {
@@ -71,8 +78,8 @@ namespace csen79 {
     };
     void Bag::print() const {
         if(isEmpty())
-            throw std::out_of_range("Cant print, stack is empty");    // If it's empty, we can't print anything
-        for (int i = top - 1; i >= 0; i--) {
+            throw std::out_of_range("Can't print, stack is empty");    // If it's empty, we can't print anything
+        for (int i = top; i >= 0; i--) {
             std::cout << data[i] << std::endl; // Printing from the top -> down
         }
     };
